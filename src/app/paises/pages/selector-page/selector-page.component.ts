ae61e279533fdb8f1c8abeb2e3ea7199ac1d1a17
Edgardo Gonzalez <gonzalezf.e@outlook.com>
2022-09-04 16:22:30 -0500

    feat: limpiando segundo selector al cambiar de region

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PaisesService } from '../../services/paises.service';
+import { PaisSmall } from '../../interfaces/paises.interface';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -10,14 +12,26 @@ import { PaisesService } from '../../services/paises.service';
 export class SelectorPageComponent implements OnInit {
   miForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
+    pais: ['', Validators.required],
   });
 
   regiones: string[] = [];
+  paises: PaisSmall[] = [];
 
   constructor(private fb: FormBuilder, private paiseService: PaisesService) {}
 
   ngOnInit(): void {
     this.regiones = this.paiseService.regiones;
+
+    this.miForm
+      .get('region')
+      ?.valueChanges.pipe(
+        tap((_) => this.miForm.get('pais')?.reset('')),
+        switchMap((region) => this.paiseService.getCountriesByRegion(region))
+      )
+      .subscribe((paises) => {
+        this.paises = paises;
+      });
   }
 
   guardar() {
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,14 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { PaisSmall } from '../interfaces/paises.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PaisesService {
+  private _baseUrl: string = 'https://restcountries.com/v2';
   private _regiones: string[] = [
     'Africa',
-    'America',
+    'Americas',
     'Asia',
-    'Europa',
+    'Europe',
     'Oceania',
   ];
 
@@ -16,5 +19,10 @@ export class PaisesService {
     return [...this._regiones];
   }
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getCountriesByRegion(region: string) {
+    const url: string = `${this._baseUrl}/region/${region}?fields=alpha3Code,name`;
+    return this.http.get<PaisSmall[]>(url);
+  }
 }
